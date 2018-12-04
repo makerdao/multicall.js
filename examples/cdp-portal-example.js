414c560656982b5ecc41f0bb010edf33ff483d3e
Michael Elliot <michael@digital.com.au>
2018-12-04 16:06:08 +0800

    minor refactor

--- examples/cdp-portal-example.js
+++ examples/cdp-portal-example.js
@@ -1,19 +1,20 @@
 import MultiCall from '../src';
-const multicall = new MultiCall('kovan');
 
-// Mainnet
+// Mainnet contract addresses
 // const SAI_TOP = '0x9b0ccf7c8994e19f39b2b4cf708e0a7df65fa8a3';
 // const SAI_TUB = '0x448a5065aebb8e423f0896e6c5d525c040f59af3';
 // const PROXY_REGISTRY = '0x4678f0a6958e4d2bc4f1baf7bc52e8f3564f3fe4';
 // const HELPER_CONTRACT = '0xfd536f5bc03ed27a240b3f80da898c9e4c33e7b1';
 // const MY_ADDRESS = '0x50104b8859824ef71413d2f9d84eea99c199cd12';
 
-// Kovan
+// Kovan contract addresses
 const SAI_TOP = '0x5f00393547561da3030ebf30e52f5dc0d5d3362c';
 const SAI_TUB = '0xa71937147b55Deb8a530C7229C442Fd3F31b7db2';
 const PROXY_REGISTRY = '0x64a436ae831c1672ae81f674cab8b6775df3475c';
 const HELPER_CONTRACT = '0x5b630ba8fe98ebed5381c868cd6b8d23875b6ca7';
 const MY_ADDRESS = '0x72776bb917751225d24c07d0663b3780b2ada67c';
+
+const multicall = new MultiCall('kovan');
 
 async function go() {
   let addresses = await getContractAddresses();

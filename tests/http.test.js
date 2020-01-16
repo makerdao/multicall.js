d4596cb86de52698ff0ebbd2372f39f67e2a4634
Michael Elliot <michael@digital.com.au>
2020-01-16 13:12:14 +0800

    Refactor tests

--- tests/http.test.js
+++ tests/http.test.js
@@ -1,47 +1,35 @@
 import { createWatcher } from '../src';
-import { calls, results } from './shared';
+import { calls, mockedResults } from './shared';
+
+const config = {
+  rpcUrl: 'https://mocked',
+  multicallAddress: '0x1234567890123456789012345678901234567890'
+};
 
 describe('http', () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
+  beforeEach(() => fetch.resetMocks());
 
   test('requests using http endpoint', async () => {
-    const config = {
-      rpcUrl: 'http://localhost:1234',
-      multicallAddress: '0x1234567890123456789012345678901234567890'
-    };
+    const results = {};
+    const watcher = createWatcher([calls[0], calls[1]], config);
+    watcher.subscribe(update => results[update.type] = update.value);
+    watcher.onNewBlock(number => results['BLOCK_NUMBER'] = number);
 
     fetch.mockResponse(async () => ({
-      body: JSON.stringify({ jsonrpc: '2.0', id: 1, result: results[0] })
+      body: JSON.stringify({ jsonrpc: '2.0', id: 1, result: mockedResults[0] })
     }));
+    await watcher.start();
 
-    const watcher = createWatcher([calls[0], calls[1], calls[2]], config);
-
-    let ethWhaleBalance, mkrWhaleBalance, priceFeedEthPrice;
-    watcher.subscribe(update => {
-      if (update.type === 'BALANCE_OF_ETH_WHALE') ethWhaleBalance = update.value;
-      else if (update.type === 'BALANCE_OF_MKR_WHALE') mkrWhaleBalance = update.value;
-      else if (update.type === 'PRICE_FEED_ETH_PRICE') priceFeedEthPrice = update.value;
-    });
-
-    let blockNumber;
-    watcher.onNewBlock(number => (blockNumber = number));
-
-    watcher.start();
-    await watcher.awaitInitialFetch();
-
-    expect(ethWhaleBalance).toEqual('1111.22223333');
-    expect(mkrWhaleBalance).toEqual('4444.55556666');
-    expect(blockNumber).toEqual(123456789);
+    expect(results['BALANCE_OF_ETH_WHALE']).toEqual('1111.22223333');
+    expect(results['BALANCE_OF_MKR_WHALE']).toEqual('4444.55556666');
+    expect(results['BLOCK_NUMBER']).toEqual(123456789);
 
     fetch.mockResponse(async () => ({
-      body: JSON.stringify({ jsonrpc: '2.0', id: 2, result: results[1] })
+      body: JSON.stringify({ jsonrpc: '2.0', id: 2, result: mockedResults[1] })
     }));
+    await watcher.tap(existing => [...existing, calls[2]]);
 
-    await watcher.tap(existing => [...existing, calls[3]]);
-
-    expect(priceFeedEthPrice).toEqual('1234.56789');
-    expect(blockNumber).toEqual(987654321);
+    expect(results['PRICE_FEED_ETH_PRICE']).toEqual('1234.56789');
+    expect(results['BLOCK_NUMBER']).toEqual(987654321);
   });
 });
--- tests/websocket.test.js
+++ tests/websocket.test.js
@@ -1,47 +1,38 @@
 import WS from 'jest-websocket-mock';
 import { createWatcher } from '../src';
-import { calls, results } from './shared';
+import { calls, mockedResults } from './shared';
+
+const config = {
+  rpcUrl: 'wss://mocked',
+  multicallAddress: '0x1234567890123456789012345678901234567890'
+};
 
 describe('websocket', () => {
-  let server;
-
   beforeAll(async () => {
-    server = new WS('ws://localhost:1234', { jsonProtocol: true });
+    const server = new WS('wss://mocked', { jsonProtocol: true });
     server.on('connection', socket => {
       socket.on('message', data => {
         const json = JSON.parse(data);
-        socket.send(JSON.stringify({ jsonrpc: '2.0', id: json.id, result: results[json.id - 1] }));
+        socket.send(JSON.stringify({ jsonrpc: '2.0', id: json.id, result: mockedResults[json.id - 1] }));
       });
     });
   });
 
   test('requests using websocket endpoint', async () => {
-    const config = {
-      rpcUrl: 'ws://localhost:1234',
-      multicallAddress: '0x1234567890123456789012345678901234567890'
-    };
-    const watcher = createWatcher([calls[0], calls[1], calls[2]], config);
+    const results = {};
+    const watcher = createWatcher([calls[0], calls[1]], config);
+    watcher.subscribe(update => results[update.type] = update.value);
+    watcher.onNewBlock(number => results['BLOCK_NUMBER'] = number);
 
-    let ethWhaleBalance, mkrWhaleBalance, priceFeedEthPrice;
-    watcher.subscribe(update => {
-      if (update.type === 'BALANCE_OF_ETH_WHALE') ethWhaleBalance = update.value;
-      else if (update.type === 'BALANCE_OF_MKR_WHALE') mkrWhaleBalance = update.value;
-      else if (update.type === 'PRICE_FEED_ETH_PRICE') priceFeedEthPrice = update.value;
-    });
+    await watcher.start();
 
-    let blockNumber;
-    watcher.onNewBlock(number => (blockNumber = number));
+    expect(results['BALANCE_OF_ETH_WHALE']).toEqual('1111.22223333');
+    expect(results['BALANCE_OF_MKR_WHALE']).toEqual('4444.55556666');
+    expect(results['BLOCK_NUMBER']).toEqual(123456789);
 
-    watcher.start();
-    await watcher.awaitInitialFetch();
+    await watcher.tap(existing => [...existing, calls[2]]);
 
-    expect(ethWhaleBalance).toEqual('1111.22223333');
-    expect(mkrWhaleBalance).toEqual('4444.55556666');
-    expect(blockNumber).toEqual(123456789);
-
-    await watcher.tap(existing => [...existing, calls[3]]);
-
-    expect(priceFeedEthPrice).toEqual('1234.56789');
-    expect(blockNumber).toEqual(987654321);
+    expect(results['PRICE_FEED_ETH_PRICE']).toEqual('1234.56789');
+    expect(results['BLOCK_NUMBER']).toEqual(987654321);
   });
 });
